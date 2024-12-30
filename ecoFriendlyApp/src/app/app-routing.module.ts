fbd04d14d6083860eb7be749d297e821a098872f
Chong Yong Xuan <103051564+CYX0410@users.noreply.github.com>
2024-12-30 21:04:05 +0800

    add profile and tracking page

--- ecoFriendlyApp/src/app/app-routing.module.ts
+++ ecoFriendlyApp/src/app/app-routing.module.ts
@@ -6,13 +6,22 @@ import { LoginPage } from './login/login.page';
 import { RegisterPage } from './register/register.page';
 import { ForgotPasswordPage } from './forgot-password/forgot-password.page';
 import { HomePage } from './home/home.page';
+import { ProfilePage } from './profile/profile.page';
+import { TrackingPage } from './tracking/tracking.page';
 
 const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'login', component: LoginPage },
   { path: 'register', component: RegisterPage },
   { path: 'forgot-password', component: ForgotPasswordPage },
-  { path: 'home', component: HomePage }
+  { path: 'home', component: HomePage },
+  {
+    path: 'profile', component:ProfilePage
+  },
+  {
+    path: 'tracking', component: TrackingPage
+  }
+
   // Other routes here...
 ];
 
